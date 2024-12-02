ba149219409f8f3820d402b44bd6241ecbf4185d
Prince Kunwar <prince.ey@tataaig.com>
2024-12-02 22:39:24 +0530

    add login page

---
+++ components/AlertDialog.vue
@@ -0,0 +1,57 @@
+<template>
+  <div
+    class="absolute items-center justify-center w-full h-screen transition-all transform shadow-lg opacity-0 duration-400 bg-slate-600/80 alert"
+  >
+    <div class="flex flex-col max-w-lg bg-white rounded-md min-h-40 min-w-96">
+      <div class="flex items-center justify-between shadow-md grow-0">
+        <h3 class="ml-4 text-base font-semibold" id="alert_title">Alert</h3>
+        <span
+          class="flex items-center h-full pl-2 pr-4 cursor-pointer"
+          id="alert_close"
+        >
+          <button class="py-2">&#10006;</button>
+        </span>
+      </div>
+      <p class="p-4 grow" id="alert_message">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam
+        aliquid placeat voluptatum ipsam officiis. Facilis, optio qui sit alias
+        earum reiciendis. Facere animi, sed possimus repellendus laudantium odit
+        fugiat vitae!
+      </p>
+      <div
+        id="alert-info-actions"
+        class="justify-center hidden px-4 pt-1 pb-3 grow-0 gap-x-4"
+      >
+        <button
+          id="alert_ok"
+          class="px-4 py-2 text-sm font-medium text-white transition-colors duration-150 bg-gray-600 rounded-md shadow-sm hover:bg-gray-700 shadow-blue-950/10 focus:outline-none focus:ring focus:ring-gray-300 focus-visible:outline-none focus-visible:ring focus-visible:ring-gray-300"
+          type="button"
+        >
+          Got, It
+        </button>
+      </div>
+
+      <div
+        id="alert-confirm-actions"
+        class="flex justify-end px-4 pt-1 pb-3 grow-0 gap-x-4"
+      >
+        <button
+          id="alert_cancel"
+          class="px-4 py-2 text-sm font-medium text-white transition-colors duration-150 bg-gray-600 rounded-md shadow-sm hover:bg-gray-700 shadow-blue-950/10 focus:outline-none focus:ring focus:ring-gray-300 focus-visible:outline-none focus-visible:ring focus-visible:ring-gray-300"
+          type="button"
+        >
+          Cancel
+        </button>
+        <button
+          id="alert_confirm"
+          class="px-4 py-2 text-sm font-medium text-white transition-colors duration-150 bg-blue-600 rounded-md shadow-sm hover:bg-blue-700 shadow-blue-950/10 focus:outline-none focus:ring focus:ring-blue-300 focus-visible:outline-none focus-visible:ring focus-visible:ring-blue-300"
+          type="button"
+        >
+          Confirm
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
---
+++ pages/login.vue
@@ -0,0 +1,104 @@
+<template>
+  <div
+    class="flex justify-center items-center h-full"
+  >
+  <div class="bg-white py-8 px-4 sm:mx-auto sm:w-full sm:max-w-md rounded-lg shadow-md">
+    <div class="sm:mx-auto sm:w-full sm:max-w-sm">
+      <img
+        class="mx-auto h-10 w-auto"
+        src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=600"
+        alt="Your Company"
+      />
+      <h2
+        class="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900"
+      >
+        Sign in to your account
+      </h2>
+    </div>
+
+    <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+      <form class="space-y-6" action="#" method="POST" @submit="onSubmitForm">
+        <div>
+          <label for="email" class="block text-sm/6 font-medium text-gray-900"
+            >Email address</label
+          >
+          <div class="mt-2">
+            <input
+              type="email"
+              name="email"
+              id="email"
+              autocomplete="email"
+              v-model="email"
+              required
+              class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+            />
+          </div>
+        </div>
+
+        <div>
+          <div class="flex items-center justify-between">
+            <label
+              for="password"
+              class="block text-sm/6 font-medium text-gray-900"
+              >Password</label
+            >
+            <div class="text-sm">
+              <a
+                href="#"
+                class="font-semibold text-indigo-600 hover:text-indigo-500"
+                >Forgot password?</a
+              >
+            </div>
+          </div>
+          <div class="mt-2">
+            <input
+              type="password"
+              name="password"
+              id="password"
+              v-model="password"
+              autocomplete="current-password"
+              required
+              class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+            />
+          </div>
+        </div>
+
+        <div>
+          <button
+            type="submit"
+            class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          >
+            Sign in
+          </button>
+        </div>
+      </form>
+
+      <p class="mt-10 text-center text-sm/6 text-gray-500">
+        Not registered?
+        {{ " " }}
+        <a href="#" class="font-semibold text-indigo-600 hover:text-indigo-500"
+          >Contact Admin Department</a
+        >
+      </p>
+    </div>
+
+
+  </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+
+const onSubmitForm = () => {
+  const paylaod = {
+    email: email.value,
+    password: password.value
+  }
+
+  console.log(paylaod);
+};
+
+const email = ref("");
+const password = ref("");
+</script>
